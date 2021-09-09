10559fb70a2d7cc637aead59fb01c9daee835c38
boelterr <bielboelter@gmail.com>
2021-09-09 16:47:39 -0300

    ex4

---
+++ ex4.py
@@ -0,0 +1,12 @@
+#Gabriel Boelter 2190
+print("Copia das listas")
+
+lista = []
+lista.append("Gabriel")
+lista.append("Thiago")
+lista.append("Augusto")
+lista.append("Elias")
+lista.append("Dj Bratti SC")
+
+lista_copia = lista
+print(lista)
