e76786b4dddc6f33ee128a7b6c7ea8049b8a181b
boelterr <bielboelter@gmail.com>
2021-09-09 17:10:20 -0300

    ex9

---
+++ ex9.py
@@ -0,0 +1,12 @@
+#Gabriel Boelter 2190
+print("Escolha 5 numeros inteiro para serem mostrados na tela")
+
+n1 = int(input("Digite o primeiro numero inteiro: "))
+n2 = int(input("Digite o segundo numero inteiro: "))
+n3 = int(input("Digite o terceiro numero inteiro: "))
+n4 = int(input("Digite o quarto numero inteiro: "))
+n5 = int(input("Digite o quinto numero inteiro: "))
+
+lista = [n1,n2,n3,n4,n5]
+
+print("A lista ordenada é: ", sorted(lista))
